1cd2374b40c25479bd16a0c6edd8110afc5bb431
pritee <pritsbidwai@gmail.com>
2018-08-25 16:13:39 +0530

    git ignore file added

---
+++ src/Ifelseladder.java
@@ -0,0 +1,15 @@
+
+public class Ifelseladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int marks=69;
+		if (marks<50){
+			
+		
+			System.out.println("fail");
+
+	}
+
+}
+}
